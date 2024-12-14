30eeb3ec52624d95aa21e9425de03ee2e6271de0
Remya <46915440+remyasreenu@users.noreply.github.com>
2024-12-14 23:24:46 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "quiz";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+?>
